17011521f3ea2dad76e0592a13afab2a9ac6c678
Aravamuthan Lakshminarayanan <l.aravamuthan@gmail.com>
2019-02-08 15:53:56 -0700

    fixes

--- search_algorithm.py
+++ search_algorithm.py
@@ -12,21 +12,22 @@ publisher = rospy.Publisher("/actions",String,queue_size =10)
 parser = argparse.ArgumentParser()
 parser.add_argument('-a',help = "Please mention algorithm to use. Default is BFS", metavar = 'bfs', action='store', dest='algorithm', default="bfs", type=str)
 
-
+def stringifyState(this, state):
+    return str(state.x) + str(state.y) + str(state.orientation);
 
 def bfs():
     init_state = problem.get_initial_state()
     goal_state = problem.get_goal_state()
-    print(init_state.x, init_state.y, init_state.orientation);
-    print(goal_state.x, goal_state.y, goal_state.orientation);
+    print("init state", stringifyState(init_state));
+    print("goal state", stringifyState(goal_state));
     possible_actions = problem.get_actions()
 
     action_list = [];
     paths = {};
-    paths[str(init_state.x) + str(init_state.y) + str(init_state.orientation)] = action_list;
+    paths[stringifyState(init_state)] = action_list;
 
     if (problem.is_goal_state(init_state)):
-        return paths[init_state];
+        return paths[stringifyState(init_state)];
     explored_states = [];
     frontier = deque();
     frontier.append(init_state);
@@ -36,12 +37,12 @@ def bfs():
 
     while frontier:
         current_state = frontier.popleft();
-        current_path = paths[str(current_state.x) + str(current_state.y) + str(current_state.orientation)];
+        current_path = paths[stringifyState(current_state)];
         explored_states.append(current_state);
         for possible_action in possible_actions:
             (nextstate, cost) = problem.get_successor(current_state,possible_action);
             if nextstate not in explored_states and nextstate not in frontier and cost > 0:
-                print(current_state.x, current_state.y, current_state.orientation,nextstate.x, nextstate.y, nextstate.orientation, possible_action);
+                print(stringifyState(current_state),stringifyState(nextstate), possible_action);
                 path_e = current_path[:];
                 path_e.append(possible_action);
                 if(problem.is_goal_state(nextstate)):
@@ -50,7 +51,7 @@ def bfs():
                     #print(explored_states);
                     return path_e;
                 frontier.append(nextstate);
-                paths[str(nextstate.x) + str(nextstate.y) + str(nextstate.orientation)] = path_e;
+                paths[stringifyState(nextstate)] = path_e;
 
     print("goal not found");
     return [];
@@ -58,19 +59,42 @@ def bfs():
 def ucs():
     init_state = problem.get_initial_state()
     goal_state = problem.get_goal_state()
-    possible_actions = problem.get_actions() 
-    action_list = []
+    print("init state", stringifyState(init_state));
+    print("goal state", stringifyState(goal_state));
+    possible_actions = problem.get_actions()
 
-    #to get the next state, cost for an action on state_x use:
+    action_list = [];
+    paths = {};
+    paths[stringifyState(init_state)] = [action_list, 0];
 
+    if (problem.is_goal_state(init_state)):
+        return paths[stringifyState(init_state)];
+    explored_states = [];
+    frontier = [];
+    heapq.heappush(frontier, (0, init_state));
 
-    '''
-    (nextstate, cost) = problem.get_successor(state, action)
-    YOUR CODE HERE
-    '''
+    # to get the next state, cost for an action on state_x use:
+    '''(nextstate, cost) = problem.get_successor(state, action)'''
 
-
-    return action_list
+    while frontier:
+        current_state = heapq.heappop(frontier);
+        (current_path, current_cost) = paths[stringifyState(current_state)];
+        explored_states.append(current_state);
+        for possible_action in possible_actions:
+            (nextstate, cost) = problem.get_successor(current_state, possible_action);
+            if nextstate not in explored_states and nextstate not in frontier and cost > 0:
+                print(stringifyState(current_state), stringifyState(nextstate), possible_action);
+                path_e = current_path[:];
+                path_e.append(possible_action);
+                if (problem.is_goal_state(nextstate)):
+                    print("goal found ");
+                    # print(frontier);
+                    # print(explored_states);
+                    return path_e;
+                heapq.heappush(frontier, (current_cost + cost, nextstate));
+                paths[stringifyState(nextstate)] = [path_e,current_cost+cost];
+    print("goal not found");
+    return [];
 
 def gbfs():
     init_state = problem.get_initial_state()
@@ -123,6 +147,3 @@ if __name__ == "__main__":
     actions = algorithm();
     print(actions);
     exec_action_list(algorithm,actions);
-
-
-
