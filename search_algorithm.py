8b5a8d2ae654365eb972ce89b27f9e24c1e49122
Aravamuthan Lakshminarayanan <l.aravamuthan@gmail.com>
2019-02-08 15:56:01 -0700

    fixes

--- search_algorithm.py
+++ search_algorithm.py
@@ -134,7 +134,7 @@ def astar():
    
 
  # to execute a plan action_list = <list of actions>, use:
-def exec_action_list(self, action_list):
+def exec_action_list(action_list):
     plan_str = '_'.join(action for action in action_list)
     publisher.publish(String(data = plan_str))
 
@@ -146,4 +146,4 @@ if __name__ == "__main__":
         exit(1)
     actions = algorithm();
     print(actions);
-    exec_action_list(algorithm,actions);
+    exec_action_list(actions);
