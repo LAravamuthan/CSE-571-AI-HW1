db06271b98e1e0c0f241983e29e372477fb6979d
Aravamuthan Lakshminarayanan <l.aravamuthan@gmail.com>
2019-02-08 20:45:25 -0700

    fixes

--- search_algorithm.py
+++ search_algorithm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import heapq
-import problem 
+import problem
 import rospy
 from std_msgs.msg import String
 import argparse
@@ -10,18 +10,22 @@ import time
 import matplotlib.pyplot as plt
 
 rospy.init_node("walk")
-publisher = rospy.Publisher("/actions",String,queue_size =10)
+publisher = rospy.Publisher("/actions", String, queue_size=10)
 parser = argparse.ArgumentParser()
-parser.add_argument('-a',help = "Please mention algorithm to use. Default is BFS", metavar = 'bfs', action='store', dest='algorithm', default="bfs", type=str)
+parser.add_argument('-a', help="Please mention algorithm to use. Default is BFS", metavar='bfs', action='store',
+                    dest='algorithm', default="bfs", type=str)
+
 
 def stringifyState(state):
     return str(state.x) + str(state.y) + str(state.orientation);
 
+
 def manhattanHeuristics(sourceState, destinationState):
     return abs(sourceState.x - destinationState.x) + abs(sourceState.y - destinationState.y);
 
+
 def euclideanHeuristics(sourceState, destinationState):
-    return ((sourceState.x - destinationState.x)**2) + ((sourceState.y - destinationState.y)**2);
+    return ((sourceState.x - destinationState.x) ** 2) + ((sourceState.y - destinationState.y) ** 2);
 
 
 def bfs():
@@ -38,13 +42,13 @@ def bfs():
 
     if (problem.is_goal_state(init_state)):
         toc = time.clock();
-        print(toc-tic);
+        print(toc - tic);
         return paths[stringifyState(init_state)];
     explored_states = [];
     frontier = deque();
     frontier.append(init_state);
 
-    #to get the next state, cost for an action on state_x use:
+    # to get the next state, cost for an action on state_x use:
     '''(nextstate, cost) = problem.get_successor(state, action)'''
 
     while frontier:
@@ -52,15 +56,15 @@ def bfs():
         current_path = paths[stringifyState(current_state)];
         explored_states.append(current_state);
         for possible_action in possible_actions:
-            (nextstate, cost) = problem.get_successor(current_state,possible_action);
+            (nextstate, cost) = problem.get_successor(current_state, possible_action);
             if nextstate not in explored_states and nextstate not in frontier and cost > 0:
-                #print(stringifyState(current_state),stringifyState(nextstate), possible_action);
+                # print(stringifyState(current_state),stringifyState(nextstate), possible_action);
                 path_e = current_path[:];
                 path_e.append(possible_action);
-                if(problem.is_goal_state(nextstate)):
+                if (problem.is_goal_state(nextstate)):
                     print("goal found ");
-                    #print(frontier);
-                    #print(explored_states);
+                    # print(frontier);
+                    # print(explored_states);
                     print(len(path_e));
                     toc = time.clock();
                     print(toc - tic);
@@ -72,6 +76,7 @@ def bfs():
     toc = time.clock();
     print(toc - tic);
     return [];
+
 
 def ucs():
     tic = time.clock();
@@ -92,9 +97,10 @@ def ucs():
     '''(nextstate, cost) = problem.get_successor(state, action)'''
 
     while frontier:
-        [current_cost, current_state, current_path]= heapq.heappop(frontier);
-        if stringifyState(current_state) in explored_states and explored_states[stringifyState(current_state)] < current_cost:
-            #print("skipped", stringifyState(current_state));
+        [current_cost, current_state, current_path] = heapq.heappop(frontier);
+        if stringifyState(current_state) in explored_states and explored_states[
+            stringifyState(current_state)] < current_cost:
+            # print("skipped", stringifyState(current_state));
             continue;
         if (problem.is_goal_state(current_state)):
             print("goal found ");
@@ -108,13 +114,14 @@ def ucs():
                 path_e = current_path[:];
                 path_e.append(possible_action);
                 totalCost = current_cost + cost;
-                #print(stringifyState(current_state), stringifyState(nextstate), possible_action);
+                # print(stringifyState(current_state), stringifyState(nextstate), possible_action);
                 heapq.heappush(frontier, (totalCost, nextstate, path_e));
         explored_states[stringifyState(current_state)] = current_cost;
     toc = time.clock();
     print(toc - tic);
     print("goal not found");
     return [];
+
 
 def gbfs():
     tic = time.clock();
@@ -149,11 +156,52 @@ def gbfs():
                 path_e.append(possible_action);
                 # print(stringifyState(current_state), stringifyState(nextstate), possible_action);
                 heapq.heappush(frontier, (manhattanHeuristics(nextstate, goal_state), nextstate, path_e));
-        explored_states[stringifyState(current_state)] = manhattanHeuristics(nextstate, goal_state);
+        explored_states[stringifyState(current_state)] = current_cost;
     toc = time.clock();
     print(toc - tic);
     print("goal not found");
     return [];
+
+
+def gbfseu():
+    tic = time.clock();
+    init_state = problem.get_initial_state()
+    goal_state = problem.get_goal_state()
+    print("init state", stringifyState(init_state));
+    print("goal state", stringifyState(goal_state));
+    possible_actions = problem.get_actions()
+
+    action_list = [];
+
+    if (problem.is_goal_state(init_state)):
+        return action_list;
+    explored_states = {};
+    frontier = [(euclideanHeuristics(init_state, goal_state), init_state, action_list)];
+
+    # to get the next state, cost for an action on state_x use:
+    '''(nextstate, cost) = problem.get_successor(state, action)'''
+
+    while frontier:
+        [current_cost, current_state, current_path] = heapq.heappop(frontier);
+        if (problem.is_goal_state(current_state)):
+            print("goal found ");
+            print(len(current_path));
+            toc = time.clock();
+            print(toc - tic);
+            return current_path;
+        for possible_action in possible_actions:
+            (nextstate, cost) = problem.get_successor(current_state, possible_action);
+            if stringifyState(nextstate) not in explored_states and cost > 0:
+                path_e = current_path[:];
+                path_e.append(possible_action);
+                # print(stringifyState(current_state), stringifyState(nextstate), possible_action);
+                heapq.heappush(frontier, (euclideanHeuristics(nextstate, goal_state), nextstate, path_e));
+        explored_states[stringifyState(current_state)] = current_cost;
+    toc = time.clock();
+    print(toc - tic);
+    print("goal not found");
+    return [];
+
 
 def astar():
     tic = time.clock();
@@ -168,7 +216,7 @@ def astar():
     if (problem.is_goal_state(init_state)):
         return action_list;
     explored_states = {};
-    frontier = [(0+manhattanHeuristics(init_state, goal_state), init_state, action_list)];
+    frontier = [(0 + manhattanHeuristics(init_state, goal_state), init_state, action_list)];
 
     # to get the next state, cost for an action on state_x use:
     '''(nextstate, cost) = problem.get_successor(state, action)'''
@@ -199,6 +247,52 @@ def astar():
     print("goal not found");
     return [];
 
+
+def astareu():
+    tic = time.clock();
+    init_state = problem.get_initial_state()
+    goal_state = problem.get_goal_state()
+    print("init state", stringifyState(init_state));
+    print("goal state", stringifyState(goal_state));
+    possible_actions = problem.get_actions()
+
+    action_list = [];
+
+    if (problem.is_goal_state(init_state)):
+        return action_list;
+    explored_states = {};
+    frontier = [(0 + euclideanHeuristics(init_state, goal_state), init_state, action_list)];
+
+    # to get the next state, cost for an action on state_x use:
+    '''(nextstate, cost) = problem.get_successor(state, action)'''
+
+    while frontier:
+        [current_cost, current_state, current_path] = heapq.heappop(frontier);
+        if stringifyState(current_state) in explored_states and explored_states[
+            stringifyState(current_state)] < current_cost:
+            # print("skipped", stringifyState(current_state));
+            continue;
+        if (problem.is_goal_state(current_state)):
+            print("goal found ");
+            print(len(current_path));
+            toc = time.clock();
+            print(toc - tic);
+            return current_path;
+        for possible_action in possible_actions:
+            (nextstate, cost) = problem.get_successor(current_state, possible_action);
+            if stringifyState(nextstate) not in explored_states and cost > 0:
+                path_e = current_path[:];
+                path_e.append(possible_action);
+                totalCost = current_cost + cost + euclideanHeuristics(nextstate, goal_state);
+                # print(stringifyState(current_state), stringifyState(nextstate), possible_action);
+                heapq.heappush(frontier, (totalCost, nextstate, path_e));
+        explored_states[stringifyState(current_state)] = current_cost;
+    toc = time.clock();
+    print(toc - tic);
+    print("goal not found");
+    return [];
+
+
 def plotResults():
     bfs_list = [0.438, 0.977, 1.736, 6.919];
     ucs_list = [0.159, 0.805, 1.321, 3.401];
@@ -221,22 +315,22 @@ def plotResults():
     plt.xlabel('dimensions');
     plt.ylabel("time taken(sec)");
     plt.show();
-   
 
- # to execute a plan action_list = <list of actions>, use:
+
+# to execute a plan action_list = <list of actions>, use:
 def exec_action_list(action_list):
     plan_str = '_'.join(action for action in action_list)
-    publisher.publish(String(data = plan_str))
+    publisher.publish(String(data=plan_str))
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
     algorithm = globals().get(args.algorithm)
     if algorithm is None:
-        print "Incorrect Algorithm name."
+        print
+        "Incorrect Algorithm name."
         exit(1)
     actions = algorithm();
     print(actions);
     exec_action_list(actions);
-    #plotResults();
-
-
+    # plotResults();
